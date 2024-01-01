924825e827cb1f546fe49d8a9ac7205809f2269c
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-01 20:43:17 +0300

    Create User Schema

--- models/User.ts
+++ models/User.ts
@@ -1,5 +1,32 @@
-import { Schema , model , models } from "mongoose"
+import { Schema, model, models } from "mongoose";
 
-const User = new Schema({
-  
-})
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			min: 3,
+			max: 20,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			max: 50,
+		},
+		password: {
+			type: String,
+			required: true,
+			min: 6,
+		},
+		img: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
+
+const User = models.User || model("User", userSchema);
+
+export default User;
