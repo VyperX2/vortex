9b79caf4a98d45e9fa2448224e9d04d8f2b9365e
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-02 13:15:46 +0300

    Setup redirects

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,11 +1,45 @@
+interface Token {
+	email: string;
+	id: string;
+}
+
+interface Session {
+	user: {
+		name: string;
+		email: string;
+		image: string;
+		id: string;
+	};
+}
+
 export const authConfig = {
 	pages: {
 		signIn: "/login",
 	},
 	providers: [],
 	callbacks: {
+		async jwt({ token, user }: { token: Token; user: any }) {
+			console.log(token);
+			if (user) {
+				token.id = user.id;
+			}
+			return token;
+		},
+		async session({ session, token }: { session: Session; token: Token }) {
+			console.log(session);
+			if (token) {
+				session.user.id = token.id;
+			}
+			return session;
+		},
 		async authorized({ auth, request }: any) {
-			console.log(auth);
+			const user = auth?.user;
+			const isOnLoginPage = request.nextUrl?.pathname.startsWith("/login");
+
+			if (isOnLoginPage && user) {
+				return Response.redirect(new URL("/", request.nextUrl));
+			}
+
 			return true;
 		},
 	},
