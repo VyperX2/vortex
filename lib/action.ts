01b5c4a3aea842e74b7680a7f528a08fa8c4fb07
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-02 08:31:46 +0300

    Encrypt password

--- lib/action.ts
+++ lib/action.ts
@@ -1,6 +1,7 @@
 import User from "@/models/User";
 import { signIn } from "./auth";
 import { connectToDB } from "./database";
+const bcrypt = require("bcrypt");
 
 interface formDataProps {
 	email: string;
@@ -29,10 +30,13 @@ export const handleRegister = async (formData: any) => {
 		const user = await User.findOne({ username });
 
 		if (!user) {
+			const salt = await bcrypt.genSalt(10);
+			const hashedPassword = await bcrypt.hash(password, salt);
+
 			const newUser = new User({
 				username,
 				email,
-				password,
+				password: hashedPassword,
 				img,
 			});
 
