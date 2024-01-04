3aed606249e9ef9a2a58f9eea5ba6969b8a92dd5
Hassan <hassanahmad2809@gmail.com>
2024-01-04 18:33:22 +0300

    responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Input } from "@/components/ui/input";
+import { IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="relative left-[500px] w-[500px]">
-      <Input />
+    <nav className="lg:ml-[300px] w-[300px] ml-6 md:w-auto py-8 flex sm:items-start">
+      <IoSearch className="absolute mt-3 ml-2 w-5 h-5 mr-2" />
+      <Input placeholder="Search for users..." className="pl-8 py-5" />
     </nav>
   );
 };
