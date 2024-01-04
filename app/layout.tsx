b264c8c51df1b1c53a0342e6df8038ba9c8c35f5
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-04 16:30:56 +0300

    Create Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@/styles/globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body className={`dark ${inter.className}`}>{children}</body>
+			<body className={`dark ${inter.className}`}>
+				<Sidebar />
+				{children}
+			</body>
 		</html>
 	);
 }
---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+	return (
+		<nav className="flex flex-col w-72 border-r fixed left-0 bottom-0 top-0 ">
+			Sidebar
+		</nav>
+	);
+};
+
+export default Sidebar;
