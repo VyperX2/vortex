66d51ade6322177452439a0f8c4f2c43f0278f6e
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-04 20:50:00 +0300

    Fix Link

--- components/SidebarLink.tsx
+++ components/SidebarLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { SidebarLinkProps } from "@/lib/types";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 const SidebarLink: React.FC<SidebarLinkProps> = ({
 	href,
@@ -11,14 +12,14 @@ const SidebarLink: React.FC<SidebarLinkProps> = ({
 	const pathname = usePathname();
 
 	return (
-		<a
+		<Link
 			className={`${pathname === href ? "bg-secondary" : ""} ${
 				index === 0 ? "border-t" : ""
 			} hover:bg-secondary pl-4 py-4 transition-all border-b  border-b-muted`}
 			href={href}
 		>
 			{title}
-		</a>
+		</Link>
 	);
 };
 
