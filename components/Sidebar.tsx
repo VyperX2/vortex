377e54d6e2a66840595062fd2412e4fc036aa7bc
Hassan <hassanahmad2809@gmail.com>
2024-01-05 00:22:31 +0300

    added footer, made profile boilerplate, didnt do responsiveness

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,34 +3,34 @@ import SidebarLink from "./SidebarLink";
 import Link from "next/link";
 
 const Sidebar = () => {
-	return (
-		<nav className="lg:flex flex-col  border-r  py-8 h-screen  hidden gap-8 w-full ">
-			<div className="flex items-center font-semibold text-lg pl-4">
-				{/* Insert ICON here */}
-				<h4>Vortex</h4>
-			</div>
+  return (
+    <nav className="lg:flex flex-col  border-r  py-8 h-screen  hidden gap-8 w-full ">
+      <div className="flex items-center font-semibold text-lg pl-4">
+        {/* Insert ICON here */}
+        <h4>Vortex</h4>
+      </div>
 
-			{/* USER PROFILE  */}
-			<Link href={"/profile"} className="flex items-center gap-4 pl-4">
-				<img
-					src="https://picsum.photos/200/300"
-					alt="profile_img"
-					className=" h-14 w-14 rounded-full"
-				/>
-				{/* CHANGE THIS TO NEXT IMAGE LATER */}
-				<div className="flex flex-col">
-					<p className="font-semibold">VyperX</p>
-					<p className="text-muted-foreground">@vyperx</p>
-				</div>
-			</Link>
+      {/* USER PROFILE  */}
+      <Link href={"/profile"} className="flex items-center gap-4 pl-4">
+        <img
+          src="https://picsum.photos/200/300"
+          alt="profile_img"
+          className=" h-14 w-14 rounded-full"
+        />
+        {/* CHANGE THIS TO NEXT IMAGE LATER */}
+        <div className="flex flex-col">
+          <p className="font-semibold">VyperX</p>
+          <p className="text-muted-foreground">@vyperx</p>
+        </div>
+      </Link>
 
-			<ul className="flex flex-col ">
-				{tabs.map((tab, index) => (
-					<SidebarLink {...tab} index={index} key={index} />
-				))}
-			</ul>
-		</nav>
-	);
+      <ul className="flex flex-col ">
+        {tabs.map((tab, index) => (
+          <SidebarLink {...tab} index={index} key={index} />
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidebar;
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,16 @@
 const tabs = [
-	{ title: "Home", href: "/", icon: "home-icon" },
-	{ title: "Explore", href: "/explore", icon: "explore-icon" },
-	{ title: "People", href: "/people", icon: "people-icon" },
-	{ title: "Saved", href: "/saved", icon: "saved-icon" },
-	{ title: "Create", href: "/create", icon: "create-icon" },
+  { title: "Home", href: "/", icon: "home-icon" },
+  { title: "Explore", href: "/explore", icon: "explore-icon" },
+  { title: "People", href: "/people", icon: "people-icon" },
+  { title: "Saved", href: "/saved", icon: "saved-icon" },
+  { title: "Create", href: "/create", icon: "create-icon" },
+];
+const Userfame = [
+  {
+    posts: 5,
+    followers: 10,
+    following: 20,
+  },
 ];
 
-export { tabs };
+export { tabs, Userfame };
