62fdee43ede94a41d487c5f978e30d6d49e3d6d4
Hassan <hassanahmad2809@gmail.com>
2024-01-04 17:57:00 +0300

    create pages

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,5 @@
-
 const LoginPage = () => {
-	return (
-		<section className="w-full h-screen flex items-center justify-center">
-		</section>
-	);
+  return <section className="w-full h-screen flex items-center justify-center"></section>;
 };
 
 export default LoginPage;
