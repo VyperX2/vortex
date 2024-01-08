0aa73aef06b5f6cf36280ec3368b39436d60d716
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-08 18:05:37 +0300

    Add UserDisplay to Menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,22 +1,19 @@
 import { IoMdMenu } from "react-icons/io";
-import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { tabs } from "@/lib/constants";
 import SidebarLink from "./SidebarLink";
+import UserDisplay from "./UserDisplay";
 
-const Menu = () => {
+const Menu = async () => {
 	return (
 		<Sheet>
 			<SheetTrigger>
 				<IoMdMenu className="absolute mt-3 ml-2 w-10 h-10 left-2 top-5 block lg:hidden text-accent " />
 			</SheetTrigger>
 			<SheetContent>
+				<div className="mb-2">
+					<UserDisplay />
+				</div>
 				<ul className="flex flex-col ">
 					{tabs.map((tab, index) => (
 						<SidebarLink {...tab} index={index} key={index} />
