1e96f5817c4d3b61752f37e8f9edeea9aeb996db
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-09 18:09:44 +0300

    Change post width

--- components/Post.tsx
+++ components/Post.tsx
@@ -7,7 +7,7 @@ const Post = () => {
 		<Link href={`/post/1`}>
 			<Image
 				src="https://picsum.photos/200/300"
-				className="rounded-lg overflow-hidden"
+				className="rounded-lg overflow-hidden 2xl:w-72 lg:w-52 md:w-52 w-72"
         height={300}
         width={200}
 				alt="post"
