2dec0f1400ff7f40f6222f295a0ef5097862301e
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-10 20:26:02 +0300

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
 
 function Error({ statusCode }) {
-  return (
-    <>
-      <p className="text-3xl">
-        {statusCode ? `An error ${statusCode} occurred on server` : "An error occurred on client"}
-      </p>
-      <Link href="/">Return to Home</Link>
-    </>
-  );
+	return (
+		<>
+			<p className="text-3xl">
+				{statusCode
+					? `An error ${statusCode} occurred on server`
+					: "An error occurred on client"}
+			</p>
+			<Link href="/">Return to Home</Link>
+		</>
+	);
 }
 
 Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
+	const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+	return { statusCode };
 };
 
 export default Error;
