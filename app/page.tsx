44c9db1ae2e3b022db860e49fdb4654aac96cd7b
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-16 19:52:01 +0300

    Create Feed and temp disable TopCreators

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
+import Feed from "@/components/Feed";
 import Navbar from "@/components/Navbar";
 import TopCreators from "@/components/TopCreators";
 
 const HomePage = () => {
 	return (
-		<div className="container flex">
-			<div className="flex-1 flex py-8">
-				<div className="flex-1">
+		<div className="container flex flex-col">
+				<div className="flex-1 h-fit py-8">
 					<Navbar />
 				</div>
-				<div className="lg:flex-[0.3] 2xl:flex-[0.2] hidden lg:block">
+				{/* <div className="lg:flex-[0.3] 2xl:flex-[0.2] hidden lg:block">
 					<TopCreators />
-				</div>
-			</div>
+				</div> */}
+			<Feed />
 		</div>
 	);
 };
---
+++ components/Feed.tsx
@@ -0,0 +1,8 @@
+
+const Feed = () => {
+  return (
+    <div className="container">Feed</div>
+  )
+}
+
+export default Feed
