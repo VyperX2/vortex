b49ec3b36149b4fa30520bc52488c8a18c66c03f
VyperX2 <mateenahmed.qatar@gmail.com>
2024-01-25 20:29:03 +0300

    Log sessions

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,6 +4,7 @@ import { Session } from "next-auth";
 
 const CreatePage = async () => {
 	const session: Session | null = await auth();
+	console.log("THIS IS SESSION PASSED AS PROP", session);
 	return (
 		<section className="flex flex-col items-center gap-8">
 			<Create session={session} />
--- components/Create.tsx
+++ components/Create.tsx
@@ -8,6 +8,7 @@ import { SingleImageDropzone } from "./single-image-dropzone";
 import { useRouter } from "next/navigation";
 import { Session } from "next-auth";
 const Create = ({ session }: { session: Session | null }) => {
+	console.log(session);
 	const [isPosting, setIsPosting] = useState<boolean>(false);
 	const [urls, setUrls] = useState<{
 		url: string;
