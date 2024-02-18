b4db9ca669112682ab5a6082e764dc0de9993d03
VyperX2 <mateenahmed.qatar@gmail.com>
2024-02-18 13:29:50 +0300

    Add link to followers

--- components/Follower.tsx
+++ components/Follower.tsx
@@ -1,14 +1,17 @@
 import { User } from "@/lib/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
-const Follower = ({ email, img, username }: User) => {
+const Follower = ({ email, img, username, _id }: User) => {
 	return (
 		<div className="py-3 px-2 transition-all rounded-md w-full flex items-center gap-2 hover:bg-secondary/25">
-			<Avatar className="h-12 w-12">
-				<AvatarImage src={img} />
-				<AvatarFallback>{username[0].toUpperCase()}</AvatarFallback>
-			</Avatar>
+			<Link href={`/profile/${_id}`}>
+				<Avatar className="h-12 w-12">
+					<AvatarImage src={img} />
+					<AvatarFallback>{username[0].toUpperCase()}</AvatarFallback>
+				</Avatar>
+			</Link>
 			<div className="flex flex-col ">
 				<p className="text-md font-semibold">{username}</p>
 				<p className="text-sm text-muted-foreground">@{email?.split("@")[0]}</p>
