b43779c5385168767cfb47f3ca4640fcf4c259fc
VyperX2 <mateenahmed.qatar@gmail.com>
2024-02-24 18:55:47 +0300

    Create Following page

--- app/profile/[id]/following/page.tsx
+++ app/profile/[id]/following/page.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { auth } from "@/lib/auth";
 import { User } from "@/lib/types";
 
-const FollowerPage = async ({ params }: { params: { id: string } }) => {
+const FollowingPage = async ({ params }: { params: { id: string } }) => {
 	const response = await fetch(
 		`http://localhost:3000/api/profile/${params.id}/followers`,
 		{ method: "GET", cache: "no-store" }
@@ -14,7 +14,7 @@ const FollowerPage = async ({ params }: { params: { id: string } }) => {
 		<div className="container grid grid-cols-1 place-items-center gap-8">
 			<Card className="w-full">
 				<CardHeader className="text-lg font-semibold ">
-					<CardTitle>Followers</CardTitle>
+					<CardTitle>Following</CardTitle>
 				</CardHeader>
 				<CardContent className="flex flex-col gap-4">
 					{data.map((follower) => (
@@ -26,4 +26,4 @@ const FollowerPage = async ({ params }: { params: { id: string } }) => {
 	);
 };
 
-export default FollowerPage;
+export default FollowingPage;
