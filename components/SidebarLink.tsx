2055c6e66bba1f3b022e7e4d20c2c680d6426c95
VyperX2 <mateenahmed.qatar@gmail.com>
2024-03-04 18:52:08 +0300

    Fix sidebar link

--- components/SidebarLink.tsx
+++ components/SidebarLink.tsx
@@ -3,6 +3,8 @@ import { SidebarLinkProps } from "@/lib/types";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Session } from "next-auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const SidebarLink = ({
 	href,
@@ -11,13 +13,16 @@ const SidebarLink = ({
 	index,
 	session,
 }: SidebarLinkProps & { session: Session }) => {
+	if (href === "/saved") {
+		href = `/saved/${session?.user?.id}`;
+	}
 	const pathname = usePathname();
 	return (
 		<Link
 			className={`${pathname === href ? "bg-secondary" : ""} ${
 				index === 0 ? "border-t" : ""
 			} hover:bg-secondary pl-4 py-4 transition-all border-b  border-b-muted flex items-center gap-4`}
-			href={href === "/saved" ? `saved/${session?.user?.id}` : href}
+			href={href}
 		>
 			{icon}
 			{title}
