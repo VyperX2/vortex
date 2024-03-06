23e8851f0d97f0ed25fa836a1b5667cbb842518d
VyperX2 <mateenahmed.qatar@gmail.com>
2024-03-06 07:26:22 +0300

    Add user to search Dialog

--- components/Follower.tsx
+++ components/Follower.tsx
@@ -1,15 +1,8 @@
 import { User } from "@/lib/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "./ui/button";
 import Link from "next/link";
 
-const Follower = async ({
-	email,
-	img,
-	username,
-	_id,
-	creatorId,
-}: User & { creatorId: string }) => {
+const Follower = async ({ email, img, username, _id }: User) => {
 	return (
 		<div className="py-3 px-2 transition-all rounded-md w-full flex items-center gap-2 hover:bg-secondary/25">
 			<Link href={`/profile/${_id}`}>
--- components/SearchUser.tsx
+++ components/SearchUser.tsx
@@ -7,12 +7,33 @@ import {
 	CommandItem,
 	CommandList,
 } from "@/components/ui/command";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { Input } from "./ui/input";
+import { User } from "@/lib/types";
+import Follower from "./Follower";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export default function SearchUser() {
 	const [open, setOpen] = useState(false);
+	const [users, setUsers] = useState<User[] | []>([]);
+
+	useEffect(() => {
+		async function getUsers() {
+			const response = await fetch("http://localhost:3000/api/people", {
+				method: "GET",
+				cache: "no-store",
+			});
+			const data = await response.json();
+			setUsers(data);
+		}
+		getUsers();
+	}, []);
+
+	useEffect(() => {
+		console.log(users);
+	}, [users]);
 
 	return (
 		<>
@@ -30,9 +51,27 @@ export default function SearchUser() {
 				<CommandList>
 					<CommandEmpty>No results found.</CommandEmpty>
 					<CommandGroup heading="Suggestions">
-						<CommandItem>Calendar</CommandItem>
-						<CommandItem>Search Emoji</CommandItem>
-						<CommandItem>Calculator</CommandItem>
+						{users.map((user) => (
+							<CommandItem>
+								{" "}
+								<Link className="w-full" href={`/profile/${user?._id}`}>
+									<div className="py-2 px-2 transition-all rounded-md w-full flex items-center gap-2 hover:bg-secondary/25">
+										<Avatar className="h-12 w-12">
+											<AvatarImage src={`${user?.img}`} />
+											<AvatarFallback>
+												{user?.username[0].toUpperCase()}
+											</AvatarFallback>
+										</Avatar>
+										<div className="flex flex-col ">
+											<p className="text-md font-semibold">{user?.username}</p>
+											<p className="text-sm text-muted-foreground">
+												@{user?.email?.split("@")[0]}
+											</p>
+										</div>
+									</div>
+								</Link>
+							</CommandItem>
+						))}
 					</CommandGroup>
 				</CommandList>
 			</CommandDialog>
