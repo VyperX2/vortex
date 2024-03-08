14395938a380a004c53f81ef0673100d462f5c69
VyperX2 <mateenahmed.qatar@gmail.com>
2024-03-08 16:21:36 +0300

    Update SavedFeed

--- app/api/people/route.ts
+++ app/api/people/route.ts
@@ -2,7 +2,7 @@ import { connectToDB } from "@/lib/database";
 import User from "@/models/User";
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 export const GET = async () => {
 	try {
 		await connectToDB();
--- components/SavedFeed.tsx
+++ components/SavedFeed.tsx
@@ -2,12 +2,16 @@
 import { useEffect, useState } from "react";
 import ProfileCard from "@/components/ProfileCard";
 import { Post, User } from "@/lib/types";
+import { Skeleton } from "./ui/skeleton";
 
 const SavedFeed = ({ params }: { params: { id: string } }) => {
 	const [posts, setPosts] = useState<Post[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
 
 	useEffect(() => {
 		const fetchPosts = async () => {
+			console.log("ABOUT TO FETCH");
+			console.log(params.id);
 			const userResponse = await fetch(
 				`https://vortex-neon.vercel.app/api/profile/${params.id}`,
 				{
@@ -31,19 +35,32 @@ const SavedFeed = ({ params }: { params: { id: string } }) => {
 
 			const postsData = await Promise.all(postPromises);
 			setPosts(postsData);
+			setIsLoading(false);
 		};
 
 		fetchPosts();
 	}, [params.id]);
 
 	return (
-		<div className="md:ml-4 mt-8 grid grid-cols-1 2xl:grid-cols-3 md:grid-cols-2 place-items-center gap-y-8">
-			{Array.isArray(posts) && posts.length > 0 ? (
-				posts.map((post) => <ProfileCard key={post._id} {...post} />)
+		<>
+			{isLoading ? (
+				<div className="flex flex-col items-center">
+					<div className="md:ml-4 mt-8 grid grid-cols-1 md:grid-cols-3 place-items-center gap-8 ">
+						<Skeleton className="w-96 h-[75vh]" />
+						<Skeleton className="w-96 h-[75vh]" />
+						<Skeleton className="w-96 h-[75vh]" />
+					</div>
+				</div>
 			) : (
-				<p>User has no saved posts</p>
+				<div className="md:ml-4 mt-8 grid grid-cols-1 2xl:grid-cols-3 md:grid-cols-2 place-items-center gap-y-8">
+					{Array.isArray(posts) && posts.length > 0 ? (
+						posts.map((post) => <ProfileCard key={post._id} {...post} />)
+					) : (
+						<p>User has no saved posts</p>
+					)}
+				</div>
 			)}
-		</div>
+		</>
 	);
 };
 
