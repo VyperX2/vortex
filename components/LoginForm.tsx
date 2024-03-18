3391ad6190a2ad16cfeeb62ddeca1d9d7a012f38
VyperX2 <mateenahmed.qatar@gmail.com>
2024-03-18 12:41:51 +0300

    Add spinner to login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -4,16 +4,29 @@ import { handleLogin } from "@/lib/actions";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { useFormState } from "react-dom";
+import { Spinner } from "./Spinner";
+import { useEffect, useState } from "react";
 
 const LoginForm = () => {
 	const [state, formAction] = useFormState(handleLogin, undefined);
+	const [loading, setLoading] = useState<boolean>(false);
+	useEffect(() => {
+		if (state?.success) {
+			setLoading(false);
+		}
+		if (state?.error) {
+			setLoading(false);
+		}
+	}, [state]);
 
 	return (
 		<form className="space-y-4" action={formAction}>
 			<p className="text-destructive"> {state?.error} </p>
 			<Input type="text" name="username" placeholder="Username" />
 			<Input type="password" name="password" placeholder="Password" />
-			<Button className="w-full">Login</Button>
+			<Button onClick={() => setLoading(true)} className="w-full">
+				{loading ? <Spinner /> : "Login"}
+			</Button>
 		</form>
 	);
 };
